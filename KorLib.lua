2c618c2e0638113a745d392a3b40a8715f8c8626
Donovan van Heerden <dp.vanheerden@gmail.com>
2022-11-04 00:30:33 +0200

    updated addon load order and included expressway font

--- KorLib.lua
+++ KorLib.lua
@@ -7,7 +7,7 @@ local AceConfig = _G.LibStub("AceConfig-3.0")
 local AceConfigDialog = _G.LibStub("AceConfigDialog-3.0")
 local AceDbOptions = _G.LibStub("AceDBOptions-3.0")
 
-K = AceAddon:NewAddon(AddOnName, "AceConsole-3.0", "AceEvent-3.0")
+K = AceAddon:NewAddon(AddOnName, "AceConsole-3.0", "AceEvent-3.0", "AceHook-3.0")
 
 K.name = AddOnName;
 K.version = GetAddOnMetadata(AddOnName, 'Version')
@@ -15,7 +15,9 @@ K.wowpatch, K.wowbuild, K.wowdate, K.wowtoc = GetBuildInfo()
 K.locale = GetLocale()
 K._Defaults = {}
 
-K.Shared = _G.LibStub("LibSharedMedia-3.0")
+K.Shared = LibStub("LibSharedMedia-3.0")
+
+K.Shared:Register("font", "Expressway", [[Interface\AddOns\KorLib\Fonts\Expressway.ttf]])
 
 _G.KorLib = K
 
@@ -24,7 +26,6 @@ function K:OnEnable()
 	self:Print("Enabled")
 
     self:ApplyStatusBarColors()
-    self:ApplyFont()
 end
 
 function K:OnInitialize()
@@ -38,6 +39,8 @@ function K:OnInitialize()
 
 	AceConfig:RegisterOptionsTable("KorLib_Profiles", profiles)
 	AceConfigDialog:AddToBlizOptions("KorLib_Profiles", "Profiles", self.name)
+
+    self:ApplyFont()
 
     self:RegisterEvents()
     self:RegisterCommands()

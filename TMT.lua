e969deeec821b79c36c364f175907495ba2a40a9
Donovan van Heerden <dp.vanheerden@gmail.com>
2022-11-04 12:46:17 +0200

    initial rebranding

--- TMT.lua
+++ TMT.lua
@@ -11,25 +11,33 @@ K = AceAddon:NewAddon(AddOnName, "AceConsole-3.0", "AceEvent-3.0", "AceHook-3.0"
 
 K.name = AddOnName;
 K.version = GetAddOnMetadata(AddOnName, 'Version')
+K.dbName = GetAddOnMetadata(AddOnName, 'X-Database')
 K.wowpatch, K.wowbuild, K.wowdate, K.wowtoc = GetBuildInfo()
 K.locale = GetLocale()
 K._Defaults = {}
+K.db = {}
 
 K.Shared = LibStub("LibSharedMedia-3.0")
 
-K.Shared:Register("font", "Expressway", [[Interface\AddOns\KorLib\Fonts\Expressway.ttf]])
+K.Shared:Register("font", "Expressway", [[Interface\AddOns\TMT\Fonts\Expressway.ttf]])
 
 _G.KorLib = K
 
 function K:OnEnable()
+    print('E - OnEnable')
+
 	self:Print("v" .. self.version)
 	self:Print("Enabled")
 
-    self:ApplyStatusBarColors()
+    if self.db.debug then
+        self:Print("debug mode enabled")
+    end
 end
 
 function K:OnInitialize()
-    self.db = _G.LibStub("AceDB-3.0"):New("KorDB", self._Defaults.InitialDb, false)
+    print('E - OnInitialize')
+
+    self.db = _G.LibStub("AceDB-3.0"):New(self.dbName, self._Defaults.InitialDb, true)
 
     AceConfig:RegisterOptionsTable(self.name, self.Options)
 
@@ -47,5 +55,5 @@ function K:OnInitialize()
 end
 
 function K:OnDisable()
-	self:Print("Disabled")
+    print('E - OnDisable')
 end
